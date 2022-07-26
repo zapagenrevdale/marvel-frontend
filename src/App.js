642261316712859bc4444cfec386843207e1e9d9
Genrev Dale Zapa <revhea2@gmail.com>
2022-07-26 19:08:15 +0800

    Create main header file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Header from "./layout/Header";
 
 const App = () => {
 
 
     return (
         <React.Fragment>
+            <Header>
 
+            </Header>
         </React.Fragment>
     );
 
