7741a4bb6c50f81030f3aea0d1e5327dea513aad
Genrev Dale Zapa <revhea2@gmail.com>
2022-07-30 12:47:56 +0800

    Create main component's rated info subcomponent
    Rated info is a component that displays the movie's rating (Motion Picture
    Association film rating system).

---
+++ src/components/main/subcomponents/RatedInfo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import style from "./RatedInfo.module.css";
+
+
+const RatedInfo = () => {
+
+
+
+    return (
+      <div className={style.info}>
+          <p className={style["rated-paragraph"]}>
+              13 +
+          </p>
+      </div>
+    );
+}
+
+export default RatedInfo;
