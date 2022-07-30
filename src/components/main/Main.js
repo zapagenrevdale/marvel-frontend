1f46cef68e769a3eaa2024949de4b0a1f6a6f2b4
Genrev Dale Zapa <revhea2@gmail.com>
2022-07-30 12:29:58 +0800

    Create Main component
    This component features a randomly selected marvel movie.

---
+++ src/components/main/Main.js
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from "react";
+import style from "./Main.module.css";
+import Summary from "./subcomponents/Summary";
+import Container from "./subcomponents/Container";
+import RatedInfo from "./subcomponents/RatedInfo";
+
+
+const Main = () => {
+
+    return (
+        <main className={style.main}>
+
+        </main>
+
+    );
+}
+
+export default Main;
