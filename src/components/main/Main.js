b5ec888f35352f190360cf5e25b280002add1ca6
Genrev Dale Zapa <revhea2@gmail.com>
2022-07-30 12:52:26 +0800

    Integrate main component's subcomponents
    - Add Iframe for the trailer of the featured movie
    - Integrate the Container, Summary, RatedInfo components
    - Update the Main component CSS style

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,9 +7,47 @@ import RatedInfo from "./subcomponents/RatedInfo";
 
 const Main = () => {
 
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const response = await fetch('https://mcuapi.herokuapp.com/api/v1/movies');
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJson = await response.json();
+            const loadedMovies = responseJson.data;
+            setMovies(loadedMovies);
+        };
+
+        fetchMovies().catch((error) => {
+            console.log(error)
+        });
+
+
+    }, []);
+
+    if (!movies || movies.length === 0){
+        return <div></div>
+    }
+
+    const featured = movies[[Math.floor(Math.random()*movies.length)]];
+
     return (
         <main className={style.main}>
-
+            <iframe className={style["background-video"]}
+                    src={`${featured["trailer_url"]}&autoplay=any&loop=1`}
+                    title="SPIDER-MAN: NO WAY HOME - Official Teaser Trailer (HD)"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+            >
+            </iframe>
+            <Container>
+                <Summary overview={featured.overview}/>
+                <RatedInfo/>
+            </Container>
         </main>
 
     );
