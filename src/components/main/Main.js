54a181de1c127f241843336531adb4a334719674
Genrev Dale Zapa <revhea2@gmail.com>
2022-08-24 19:14:29 +0800

    Implement the movie context provider in Main.js
    Minor updates also include the following:
         a. Formatting using prettier
         b. Change in API connection (the author created a custom
             backend API) for marvel movies data fetching
        c. Separation of imported modules

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,56 +1,88 @@
-import React, {useEffect, useState} from "react";
+import React, { useContext, useEffect } from "react";
+import useHTTP from "../../hooks/UseHTTP";
+import MovieContext from "../../context-provider/movie-context";
+// component-related imports
 import style from "./Main.module.css";
 import Summary from "./subcomponents/Summary";
 import Container from "./subcomponents/layout/Container";
 import RatedInfo from "./subcomponents/RatedInfo";
 
-
 const Main = () => {
+  const movieContext = useContext(MovieContext);
+  const {
+    loading: fetchMoviesLoading,
+    error: fetchMoviesError,
+    sendRequest: fetchMovies,
+  } = useHTTP();
 
-    const [movies, setMovies] = useState([]);
+  const {
+    loading: fetchFeaturedLoading,
+    error: fetchFeaturedError,
+    sendRequest: fetchFeatured,
+  } = useHTTP();
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            const response = await fetch('https://mcuapi.herokuapp.com/api/v1/movies');
+  useEffect(() => {
+    const setMovies = (movies) => {
+      movieContext.dispatchMovieAction({
+        type: "SET_MOVIES",
+        payload: { movies },
+      });
+    };
 
-            if (!response.ok) {
-                throw new Error('Something went wrong!');
-            }
+    const setFeatured = (movie) => {
+      movieContext.dispatchMovieAction({
+        type: "SET_FEATURED",
+        payload: { featuredMovie: movie },
+      });
+    };
 
-            const responseJson = await response.json();
-            const loadedMovies = responseJson.data;
-            setMovies(loadedMovies);
-        };
+    fetchMovies(
+      {
+        url: "http://localhost:5000/marvel",
+        method: "GET",
+      },
+      setMovies
+    ).catch((e) => console.log(e.message || "Error on React hook"));
 
-        fetchMovies().catch((error) => {
-            console.log(error)
-        });
+    fetchFeatured(
+      {
+        url: "http://localhost:5000/marvel/featured",
+        method: "GET",
+      },
+      setFeatured
+    ).catch((e) => console.log(e.message || "Error on React hook"));
+  }, []);
 
+  if (movieContext.movies.length === 0 || !movieContext.featuredMovie) {
+    return <div></div>;
+  }
 
-    }, []);
+  const featured = movieContext.featuredMovie;
+  let trailer_url = featured["trailer_url"];
 
-    if (!movies || movies.length === 0){
-        return <div></div>
-    }
+  if (trailer_url.includes("youtu.be")) {
+    trailer_url = trailer_url.replace("youtu.be/", "youtube.com/embed/");
+    const featuredMovieID = trailer_url.split("embed/")[1];
+    trailer_url += `?rel=0&amp;autoplay=1&mute=1&loop=1&playlist=${featuredMovieID}`;
+  } else {
+    trailer_url += "&muted&autoplay&loop=1";
+  }
 
-    const featured = movies[[Math.floor(Math.random()*movies.length)]];
-
-    return (
-        <main className={style.main}>
-            <iframe className={style["background-video"]}
-                    src={`${featured["trailer_url"]}&autoplay=any&loop=1`}
-                    title="SPIDER-MAN: NO WAY HOME - Official Teaser Trailer (HD)"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-            >
-            </iframe>
-            <Container>
-                <Summary overview={featured.overview}/>
-                <RatedInfo/>
-            </Container>
-        </main>
-
-    );
-}
+  return (
+    <main className={style.main}>
+      <iframe
+        className={style["background-video"]}
+        src={`${trailer_url}`}
+        title="SPIDER-MAN: NO WAY HOME - Official Teaser Trailer (HD)"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <Container>
+        <Summary overview={featured.imdb["Plot"]} />
+        <RatedInfo rating={featured.imdb["Rated"]} />
+      </Container>
+    </main>
+  );
+};
 
 export default Main;
