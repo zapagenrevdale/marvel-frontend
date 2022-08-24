1f06fd0d998465bef890f41d781467377668a635
Genrev Dale Zapa <revhea2@gmail.com>
2022-08-24 19:23:52 +0800

    Integrate context provider and MovieList component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Header from "./layout/Header";
 import Main from "./components/main/Main";
+import MovieProvider from "./context-provider/MovieProvider";
+import MovieList from "./components/movie-list/MovieList"
 
 const App = () => {
-
-    return (
-        <React.Fragment>
-            <Header/>
-            <Main />
-        </React.Fragment>
-    );
-
-}
-
+  return (
+    <React.Fragment>
+      <Header />
+      <MovieProvider>
+        <Main />
+        <MovieList />
+      </MovieProvider>
+    </React.Fragment>
+  );
+};
 
 export default App;
