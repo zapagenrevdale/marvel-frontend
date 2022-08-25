0f1c7b85d511083f6beaf5bb0e0f9a41865a164f
Genrev Dale Zapa <revhea2@gmail.com>
2022-08-25 19:16:37 +0800

    Integrate TopMovies and utilize movieContext

--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useContext} from "react";
 import Container from "./subcomponent/Container";
 import ShiftedContainer from "./subcomponent/ShiftedContainer";
-
+import TopMovies from "./subcomponent/top-movies/TopMovies";
+import MovieContext from "../../context-provider/movie-context";
 const MovieList = () => {
+
+  const movieContext = useContext(MovieContext);
+
+  if(movieContext.movies.length === 0){
+    return <div></div>
+  }
   return (
     <Container>
       <ShiftedContainer>
-        
+        <TopMovies movies={movieContext.movies}/>
       </ShiftedContainer>
     </Container>
   );
