37c1b617131e2ea4276290b7a9ab852045b39a9f
Genrev Dale Zapa <revhea2@gmail.com>
2022-08-26 21:46:46 +0800

    Add movie list groupings
    Groupings are currently done in front-end because there are currently
    only 39 marvel movies made. Hence, it would not be beneficial to call
    the backend to do the groupings.

--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -1,19 +1,41 @@
-import React, {useContext} from "react";
+import React, { useContext, useMemo } from "react";
 import Container from "./subcomponent/Container";
 import ShiftedContainer from "./subcomponent/ShiftedContainer";
-import TopMovies from "./subcomponent/top-movies/TopMovies";
+import MovieGroupContainer from "./subcomponent/MovieGroupContainer";
 import MovieContext from "../../context-provider/movie-context";
 const MovieList = () => {
-
   const movieContext = useContext(MovieContext);
 
-  if(movieContext.movies.length === 0){
-    return <div></div>
+  // Added useMemo just in case another state is added in the future
+  const topMovies = useMemo(() => {
+    return movieContext.movies
+      .sort((movie1, movie2) => parseFloat(movie2.imdb.Metascore) - parseFloat(movie1.imdb.Metascore))
+      .slice(0, 25);
+  }, [movieContext.movies]);
+
+  // Added useMemo just in case another state is added in the future
+  const inFinitySaga = useMemo(() => {
+    return movieContext.movies.filter(
+      (movie) => movie.saga === "Infinity Saga"
+    );
+  }, [movieContext.movies]);
+
+  const multiverseSaga = useMemo(() => {
+    return movieContext.movies.filter(
+      (movie) => movie.saga === "Multiverse Saga"
+    );
+  }, [movieContext.movies]);
+
+  if (movieContext.movies.length === 0) {
+    return <div></div>;
   }
+
   return (
     <Container>
       <ShiftedContainer>
-        <TopMovies movies={movieContext.movies}/>
+        <MovieGroupContainer movies={topMovies} title={"Top 25 Marvel Movies"} />
+        <MovieGroupContainer movies={inFinitySaga} title={"Infinity Saga"} />
+        <MovieGroupContainer movies={multiverseSaga} title={"Multiverse Saga"} />
       </ShiftedContainer>
     </Container>
   );
