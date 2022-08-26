895760adac26abe284f20c5718ce05143af8d693
Genrev Dale Zapa <revhea2@gmail.com>
2022-08-26 23:16:12 +0800

    Remove console debugging

--- src/components/shared/MovieCard.js
+++ src/components/shared/MovieCard.js
@@ -3,7 +3,6 @@ import style from "./MovieCard.module.css";
 
 const MovieCard = (props) => {
   
-  console.log("rendered")
   return (
     <div className={style.card}>
       <img className={style.image} src={props.movie.cover_url} alt={props.movie.title} />
