c2a6371ee767888f3ae529847322624330adadbe
zapagenrevdale <zapagenrevdale@gmail.com>
2022-09-01 14:22:12 +0800

    Apply modal context on Summary.js component

--- src/components/main/subcomponents/Summary.js
+++ src/components/main/subcomponents/Summary.js
@@ -1,24 +1,26 @@
-import React from "react";
+import React, {useContext} from "react";
 import style from "./Summary.module.css";
-import Button from "./buttons/Button";
+import Button from "../../shared/buttons/Button";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faPlay, faCircleInfo} from '@fortawesome/free-solid-svg-icons'
+import ModalContext from "../../../context-provider/modal-context";
 
 const Summary = (props) => {
 
+    const modalContext = useContext(ModalContext);
     return (
         <section className={style.summary}>
             <h1 className={style.summary__Title}>
-                {props.title}
+                {props.featured.imdb.Title}
             </h1>
             <p className={style.summary__Text}>
-                {props.overview}
+                {props.featured.imdb.Plot}
             </p>
             <div className={style["button-container"]}>
                 <Button type="play">
                     <FontAwesomeIcon icon={faPlay} className={style.icon}/> Play
                 </Button>
-                <Button type="details">
+                <Button type="details" method={ () => modalContext.toggleModal(props.featured)}>
                     <FontAwesomeIcon icon={faCircleInfo} className={style.icon}/> More Info
                 </Button>
             </div>
