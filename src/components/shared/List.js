adea4fd2d99be11d17cd9beffb78d519aea81c5c
zapagenrevdale <zapagenrevdale@gmail.com>
2022-09-01 14:29:06 +0800

    Add modal toggle on  movie card component

--- src/components/shared/List.js
+++ src/components/shared/List.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import MovieCard from "./MovieCard";
+import ModalContext from "../../context-provider/modal-context";
 
 const List = (props) => {
+  const modalContext = useContext(ModalContext);
+
   return (
     <>
       {props.movies.map((movie) => (
-        <MovieCard movie={movie} key={movie.id} />
+        <MovieCard
+          movie={movie}
+          key={movie.id}
+          toggleModal={() => modalContext.toggleModal(movie)}
+        />
       ))}
     </>
   );
