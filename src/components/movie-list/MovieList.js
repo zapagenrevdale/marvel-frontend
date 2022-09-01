c92c7e1fe271e9b1bf8d15e65bfbf5c1c60353a4
zapagenrevdale <zapagenrevdale@gmail.com>
2022-09-01 14:27:29 +0800

    Add modal component and refactor code (css&format)

--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -1,23 +1,33 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext, useMemo} from "react";
 import Container from "./subcomponent/Container";
 import ShiftedContainer from "./subcomponent/ShiftedContainer";
 import MovieGroupContainer from "./subcomponent/MovieGroupContainer";
 import MovieContext from "../../context-provider/movie-context";
+import Modal from "../shared/modal/Modal";
+import ModalContext from "../../context-provider/modal-context"
+
 const MovieList = () => {
+  
+  const modalContext = useContext(ModalContext)
   const movieContext = useContext(MovieContext);
 
   // Added useMemo just in case another state is added in the future
   const topMovies = useMemo(() => {
     return movieContext.movies
-      .sort((movie1, movie2) => parseFloat(movie2.imdb.Metascore) - parseFloat(movie1.imdb.Metascore))
+      .sort(
+        (movie1, movie2) =>
+          parseFloat(movie2.imdb.Metascore) - parseFloat(movie1.imdb.Metascore)
+      )
       .slice(0, 25);
   }, [movieContext.movies]);
 
   // Added useMemo just in case another state is added in the future
   const inFinitySaga = useMemo(() => {
-    return movieContext.movies.filter(
-      (movie) => movie.saga === "Infinity Saga"
-    ).sort((movie1, movie2) => movie2.release_date > movie1.release_date? -1: 1);
+    return movieContext.movies
+      .filter((movie) => movie.saga === "Infinity Saga")
+      .sort((movie1, movie2) =>
+        movie2.release_date > movie1.release_date ? -1 : 1
+      );
   }, [movieContext.movies]);
 
   const multiverseSaga = useMemo(() => {
@@ -26,6 +36,7 @@ const MovieList = () => {
     );
   }, [movieContext.movies]);
 
+
   if (movieContext.movies.length === 0) {
     return <div></div>;
   }
@@ -33,10 +44,20 @@ const MovieList = () => {
   return (
     <Container>
       <ShiftedContainer>
-        <MovieGroupContainer movies={topMovies} title={"Top 25 Marvel Movies"} />
-        <MovieGroupContainer movies={inFinitySaga} title={"Infinity Saga"} />
-        <MovieGroupContainer movies={multiverseSaga} title={"Multiverse Saga"} />
+        <MovieGroupContainer
+          movies={topMovies}
+          title={"Top 25 Marvel Movies"}
+        />
+        <MovieGroupContainer
+          movies={inFinitySaga}
+          title={"Infinity Saga"}
+        />
+        <MovieGroupContainer
+          movies={multiverseSaga}
+          title={"Multiverse Saga"}
+        />
       </ShiftedContainer>
+      {modalContext.show && <Modal movie={modalContext.movie}/>}
     </Container>
   );
 };
