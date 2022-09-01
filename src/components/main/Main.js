df9796bad6e19c02360015fc894e104b6b6ce2ad
zapagenrevdale <zapagenrevdale@gmail.com>
2022-09-01 14:24:11 +0800

    Change arguments on Summary and RatedInfo

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -78,8 +78,8 @@ const Main = () => {
         allowFullScreen
       ></iframe>
       <Container>
-        <Summary overview={featured.imdb["Plot"]} title={featured.imdb.Title}/>
-        <RatedInfo rating={featured.imdb["Rated"]} />
+        <Summary featured={featured}/>
+        <RatedInfo rating={featured.imdb.Rated} />
       </Container>
     </main>
   );
